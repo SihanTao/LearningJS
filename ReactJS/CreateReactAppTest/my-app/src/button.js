18eb2edc42f3e5062414daa8066b3bf6a51ce456
Sihan Tao JMC UG <sihan.tao20@imperial.ac.uk>
2022-06-04 18:11:31 +0100

    Add some images

---
+++ ReactJS/CreateReactAppTest/my-app/src/button.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+  render() {
+    // ...
+    return (
+        <button value={"I'm a button"}>I'm a button</button>
+    );
+  }
+}
+
+export default Button; // Don’t forget to use export default!
