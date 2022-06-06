a350ad349c7232bf8416c881aa929edb7824012a
Sihan Tao JMC UG <sihan.tao20@imperial.ac.uk>
2022-06-06 16:04:19 +0100

    Basic Clock

--- ReactNative/AwesomeProject/App.js
+++ ReactNative/AwesomeProject/App.js
@@ -1,14 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Clock from './Clock';
-import YourApp from './helloWorld';
+import HelloWorldApp from './helloWorld';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>My Name is Sihan Tao</Text>
-      <YourApp />
+      <HelloWorldApp />
       {/* <Clock /> */}
       <StatusBar style="auto" />
     </View>
